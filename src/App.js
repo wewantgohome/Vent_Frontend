6d1636ed8be43420d9ed01161f33151ae3ba2a27
Jamkris <dltmdgus1412@gmail.com>
2024-01-28 02:00:48 +0900

    ADD :: 이벤트 상세페이지 퍼블리싱 완료

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Page404 from './page/Page404/Page404.jsx'
 import Signin from './page/Signin/Signin.jsx'
 import Signup from './page/Signup/Signup.jsx'
 import CreateEvent from './page/CreateEvent/CreateEvent.jsx';
+import Envent from './page/Event/Envent.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/create" element={<CreateEvent />}></Route>
+      <Route path="/event" element={<Envent />}></Route>
       <Route path="/signin" element={<Signin />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
       <Route path="*" element={<Page404 />}></Route>
---
+++ src/page/Event/style.js
@@ -0,0 +1,187 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0px 22%;
+`
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const Event_Img = styled.img`
+
+`
+
+export const Detail = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`
+
+export const Detail_Title = styled.div`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+
+export const Period_Body = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const Period_Title = styled.div`
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+export const Period_Detail = styled.div`
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+export const Organizer_Body = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const Organizer_Title = styled.div`
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+export const Organizer_Detail = styled.div`
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+export const OnOffline_Body = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const OnOffline_Title = styled.div`
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+export const OnOffline_Detail = styled.div`
+  color: #191919;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: black;
+`
+
+export const Amount = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: -10px 0 -30px 0;
+`
+
+export const Amount_Text = styled.div`
+  color: var(--Grayscale-500, #434149);
+  font-family: "Pretendard Variable";
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px;
+  letter-spacing: -0.24px;
+`
+
+export const Amount_Price = styled.div`
+  color: var(--Primary, #5B22FF);
+  font-family: "Pretendard Variable";
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px; 
+  letter-spacing: -0.24px;
+`
+
+export const Calculate = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const Calculate_Day = styled.div`
+  color: var(--Grayscale-400, #615E69);
+  font-family: "Pretendard Variable";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.16px;
+`
+
+export const Calculate_Price = styled.div`
+  color: var(--Grayscale-400, #615E69);
+  font-family: "Pretendard Variable";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.16px;
+`
+
+export const Desc = styled.div`
+  color: #191919;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  margin-top: 30px;
+`
