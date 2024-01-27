293a3f166f52feaca6b2961cedd2dbdaf981a2e3
Jamkris <dltmdgus1412@gmail.com>
2024-01-27 20:30:57 +0900

    ADD :: 회원가입 완성

--- src/page/Signup/Signup.jsx
+++ src/page/Signup/Signup.jsx
@@ -1,12 +1,69 @@
 import React from 'react';
-import "./style";
+import * as S from './style';
+
+import Input from '../../components/Input/Input.jsx';
+import Button from '../../components/Button/Button.jsx';
 
 const Signup = () => {
-  return (
-    <div>
-      SignUp
-    </div>
-  );
+	return (
+		<S.Container>
+			<S.Logo_Img src='../../../Img/Logo.svg' />
+			<S.Input_Box>
+				<S.Input_Text>아이디</S.Input_Text>
+				<Input
+					type={'text'}
+					holder={'아이디를 입력해주세요'}
+					onClickMethod={() => {}}
+				/>
+			</S.Input_Box>
+
+			<S.Input_Box>
+				<S.Input_Text>비밀번호</S.Input_Text>
+				<Input
+					type={'password'}
+					holder={'비밀번호를 입력해주세요'}
+					onClickMethod={() => {}}
+				/>
+			</S.Input_Box>
+
+			<S.Input_Box>
+				<S.Input_Text>비밀번호 확인</S.Input_Text>
+				<Input
+					type={'password'}
+					holder={'비밀번호를 한번 더 입력해주세요'}
+					onClickMethod={() => {}}
+				/>
+			</S.Input_Box>
+
+      <S.Input_Box>
+				<S.Input_Text>회사명</S.Input_Text>
+				<Input
+					type={'text'}
+					holder={'회사명(기업명)을 입력해주세요'}
+					onClickMethod={() => {}}
+				/>
+			</S.Input_Box>
+
+      <S.Input_Box>
+				<S.Input_Text>사업자 등록번호</S.Input_Text>
+				<Input
+					type={'text'}
+					holder={'사업자 등록번호를 입력해주세요'}
+					onClickMethod={() => {}}
+				/>
+			</S.Input_Box>
+
+			<S.Button_Box>
+				<Button text={'회원가입'} onClickMethod={() => {}} />
+			</S.Button_Box>
+
+			<S.Link_Box>
+				<S.RegisterLink href='/signin'>
+					Vent Enterprise 로그인
+				</S.RegisterLink>
+			</S.Link_Box>
+		</S.Container>
+	);
 };
 
 export default Signup;
--- src/page/Signup/style.js
+++ src/page/Signup/style.js
@@ -1,1 +1,48 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 6% 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+`
+
+export const Input_Box = styled.div`
+  margin-top: 10%;
+`
+
+export const Input_Text = styled.div`
+  margin-bottom: 3%;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.16px;
+`
+
+export const Logo_Img = styled.img`
+  width: 265px;
+`
+
+export const Button_Box = styled.div`
+  margin-top: 10%;
+`
+
+export const Link_Box = styled.div`
+  margin-top: 4%;
+  width: max-content;
+`
+
+export const RegisterLink = styled.a`
+  width: 56px;
+  color: var(--Primary, #5B22FF);
+  font-family: "Pretendard Variable";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: -0.16px;
+  text-decoration-line: underline;
+`
