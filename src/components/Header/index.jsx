4170a12c4d57bacc18b5881497258c782cc0d765
Jamkris <dltmdgus1412@gmail.com>
2024-01-28 10:56:28 +0900

    ADD :: Logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,33 +1,38 @@
-import React from "react";
-import * as S from "./style";
-import { MenuData } from "../../asset/data/MenuData";
-import { useLocation, useNavigate } from "react-router-dom";
+import React from 'react';
+import * as S from './style';
+import { MenuData } from '../../asset/data/MenuData';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 const Header = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const Logout = () => {};
-  return (
-    <S.Wrapper>
-      <S.Layout>
-        <S.LogoWrapper onClick={() => navigate("/")}>
-          <img src="../../../Img/Logo.svg" />
-        </S.LogoWrapper>
-        <S.MenuList>
-          {MenuData.map((i) => (
-            <S.MenuItem
-              key={i.value}
-              href={i.value}
-              onPage={location.pathname === i.value}
-            >
-              {i.name}
-            </S.MenuItem>
-          ))}
-          <S.Logout onClick={Logout}>로그아웃</S.Logout>
-        </S.MenuList>
-      </S.Layout>
-    </S.Wrapper>
-  );
+	const location = useLocation();
+	const navigate = useNavigate();
+  const [, , removeCookie] = useCookies(["accessToken"]);
+	const Logout = () => {
+		removeCookie("accessToken", { path: '/' });
+    navigate('/signin')
+	};
+	return (
+		<S.Wrapper>
+			<S.Layout>
+				<S.LogoWrapper onClick={() => navigate('/')}>
+					<img src='../../../Img/Logo.svg' />
+				</S.LogoWrapper>
+				<S.MenuList>
+					{MenuData.map((i) => (
+						<S.MenuItem
+							key={i.value}
+							href={i.value}
+							onPage={location.pathname === i.value}
+						>
+							{i.name}
+						</S.MenuItem>
+					))}
+					<S.Logout onClick={Logout}>로그아웃</S.Logout>
+				</S.MenuList>
+			</S.Layout>
+		</S.Wrapper>
+	);
 };
 
 export default Header;
